133b6846349beaafb14ce8059ff419240565ff41
MonserratPime <68214193+MonserratPime@users.noreply.github.com>
2023-10-12 01:56:18 -0600

    Update formatearFecha.jsx

--- src/helpers/formatearFecha.jsx
+++ src/helpers/formatearFecha.jsx
@@ -1,3 +1,7 @@
+/*
+    Funcionalidad: toma una fecha en formato ISO 8601, la divide y la formatea en un formato legible,
+    mostrando la fecha en el idioma español y un formato largo que incluye el día de la semana, el año, el mes y el día.
+*/
 export const formatearFecha = fecha => {
     const nuevaFecha = new Date(fecha.split('T')[0].split('-'))
     const opciones = {
